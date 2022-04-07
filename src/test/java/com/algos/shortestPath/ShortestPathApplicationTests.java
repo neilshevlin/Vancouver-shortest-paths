9dbf0c4713a44645cd8d3430bbad9901f2c7a71f
Neil Shevlin <neilshevlin.98@gmail.com>
2022-04-07 15:46:34 +0100

    add Shortest path app

---
+++ src/test/java/com/algos/shortestPath/ShortestPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algos.shortestPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortestPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
