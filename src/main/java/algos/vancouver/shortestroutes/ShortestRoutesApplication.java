18c40b2b60d1e0284c1281a9132c77994728a0db
Neil Shevlin <neilshevlin.98@gmail.com>
2022-04-10 18:08:52 +0100

    Add Shortest Routes application

---
+++ src/main/java/algos/vancouver/shortestroutes/ShortestRoutesApplication.java
@@ -0,0 +1,13 @@
+package algos.vancouver.shortestroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortestRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortestRoutesApplication.class, args);
+	}
+
+}
